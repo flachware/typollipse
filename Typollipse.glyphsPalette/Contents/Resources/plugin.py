230d73018325385f69afb1c34b790897b4d9c4b7
Flachware <mail@flachware.com>
2023-06-01 13:49:50 +0200

    Test actions

--- Typollipse.glyphsPalette/Contents/Resources/plugin.py
+++ Typollipse.glyphsPalette/Contents/Resources/plugin.py
@@ -20,55 +20,28 @@ class typollipse (PalettePlugin):
 
 	dialog = objc.IBOutlet()
 	textField = objc.IBOutlet()
-
+	
 	@objc.python_method
 	def settings(self):
 		self.name = Glyphs.localize({
 			'en': 'Typollipse',
 			'de': 'Typollipse',
 			'fr': 'Typollipse',
-			'es': 'Typollipse',
-			'pt': 'Typollipse',
+			'es': 'Tipolipse',
+			'pt': 'Tipolipse',
 			})
 
 		# Load .nib dialog (without .extension)
 		self.loadNib('IBdialog', __file__)
 
-	@objc.python_method
-	def start(self):
-		# Adding a callback for the 'GSUpdateInterface' event
-		Glyphs.addCallback(self.update, UPDATEINTERFACE)
-
-	@objc.python_method
-	def __del__(self):
-		Glyphs.removeCallback(self.update)
-
-	@objc.python_method
-	def update(self, sender):
-
-		text = []
-		# Extract font from sender
-		currentTab = sender.object()
-		# We’re in the Edit View
-		if isinstance(currentTab, GSEditViewController):
-			# Check whether glyph is being edited
-			layer = currentTab.activeLayer()
-			if layer is not None:
-				text.append('Selected nodes: %s' % len(layer.selection))
-				if layer.selection:
-					text.append('Selection bounds: %sx%s' % (int(layer.selectionBounds.size.width), int(layer.selectionBounds.size.height)))
-
-		# We’re in the Font view
-		else:
-			try:
-				text.append('Selected glyphs: %s' % len(currentTab.selectedLayers))
-			except:
-				pass
-
-		# Send text to dialog to display
-		self.textField.setStringValue_('\n'.join(text))
+	@objc.IBAction
+	def setAnisotropy_(self, sender):
+		self.logToConsole(sender.floatValue())
+	
+	@objc.IBAction
+	def setCurvature_(self, sender):
+		self.logToConsole('Button clicked')
 
 	@objc.python_method
 	def __file__(self):
-		"""Please leave this method unchanged"""
 		return __file__
